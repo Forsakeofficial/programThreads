fc1ff55ee8e55c93913fb6da373119be5741d931
kubah <kubahoque@gmail.com>
2022-11-05 12:45:57 +0100

    New commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         Thread thread1 = new Thread(()->{
-            System.out.println("Start wątku");
+            System.out.println("Start wątku 1");
             for (int i = 0; i < 10; i++){
-                System.out.println("Krok: " + i);
+                System.out.println("[Thread 1] Krok: " + i);
             }
-            System.out.println("Koniec wątku");
-
-            System.out.println("Start wątku");
-            for (int i = 10; i < 20; i++){
-                System.out.println("Krok: " + i);
+            System.out.println("Koniec wątku 1");
+        });
+        Thread thread2 = new Thread(()->{
+            System.out.println("Start wątku 2");
+            for (int i = 0; i < 10; i++){
+                System.out.println("[Thread 2] Krok: " + i);
             }
-            System.out.println("Koniec wątku");
-
-            System.out.println("Start wątku");
-            for (int i = 20; i < 30; i++){
-                System.out.println("Krok: " + i);
+            System.out.println("Koniec wątku 2");
+        });
+        Thread thread3 = new Thread(()->{
+            System.out.println("Start wątku 3");
+            for (int i = 0; i < 10; i++){
+                System.out.println("[Thread 3] Krok: " + i);
             }
-            System.out.println("Koniec wątku");
+            System.out.println("Koniec wątku 3");
         });
         thread1.start();
+        thread2.start();
+        thread3.start();
 
 
     }
